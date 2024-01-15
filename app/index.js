3d202f83e1aaeb0d18166b324212211aa2c021df
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2024-01-15 15:12:22 +0700

    Feat: Home

--- app/index.js
+++ app/index.js
@@ -1,13 +1,46 @@
-import { View, Text } from "react-native";
+import { Text, SafeAreaView, ScrollView, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { COLORS, images, icons, SIZES } from "../constants";
+import { Nearbyjobs, ScreenHeaderBtn, Welcome, Popularjobs } from "../components"
 
 
 
 export default function Home() {
+    const router = useRouter()
+
     return (
-        <View>
-            <Text>
-                Home
-            </Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={
+                    {
+                        headerStyles: { backgroundColor: COLORS.lightWhite },
+                        headerShadowVisible: true,
+                        headerLeft: () => (
+                            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                        ),
+                        headerRight: () => (
+                            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                        ),
+                        headerTitle: ""
+                    }
+                }
+            />
+
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+            >
+
+                <View
+                    style={{ flex: 1, padding: SIZES.medium }}
+                >
+                    <Welcome 
+                    
+                    />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,13 +1,68 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native'
 
+import { SIZES, icons } from '../../../constants'
 import styles from './welcome.style'
+import { useRouter } from 'expo-router'
 
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time")
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello T</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            onChange={() => console.log("TODO")}
+            value=''
+            placeholder='What are you looking for?'
+          />
+        </View>
+        <TouchableOpacity style={styles} onPress={() => console.log("TODO PressSearch")}>
+          <Image
+            source={icons.search}
+            resizeMode='cover'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          keyExtractor={item => item}
+          horizontal
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </View >
   )
 }
 
